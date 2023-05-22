27e0bfecd1e4e5e14e40bd0177731a3d93f01946
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-22 15:55:33 +0200

    add card component in card list

---
+++ src/components/CardList.vue
@@ -0,0 +1,18 @@
+<script>
+import Card from "./Card.vue";
+import { store } from "../store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  components: {
+    Card,
+  },
+};
+</script>
+<template></template>
+
+<style lang="scss" scoped></style>
