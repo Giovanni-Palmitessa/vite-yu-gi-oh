c7807d044a35e4ce2eb09babbb2469c39a909fb4
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-22 16:25:58 +0200

    test for images

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,7 +7,7 @@ export default {
 </script>
 <template>
   <div class="card">
-    <img :src="cardData.card_images.image_url_small" :alt="cardData.name" />
+    <img :src="cardData.card_images.image_url" :alt="cardData.name" />
     <h2>{{ cardData.name }}</h2>
     <div class="type">{{ cardData.archetype }}</div>
   </div>
@@ -15,7 +15,7 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-  width: calc((100% - 4rem) / 6);
+  width: calc((100% - 4rem) / 5);
   border: 1px solid brown;
   img {
     width: 100%;
