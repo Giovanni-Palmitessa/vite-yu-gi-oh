d940988d846d4e35166883d004ce355373d855bc
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-22 15:13:39 +0200

    complete Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,8 +5,22 @@ export default {};
   <header>
     <div class="navbar">
       <img src="../../public/plugin.video.YU-GI-OH.png" alt="Yu Gi Oh!" />
+
+      <span>Yu-Gi-Oh! API</span>
     </div>
   </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.navbar {
+  height: 100px;
+  font-size: 3rem;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  img {
+    height: 100%;
+  }
+}
+</style>
