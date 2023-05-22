24d1e64e7c2adbf840cfda1c2ab4155b454970db
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-22 15:33:38 +0200

    complete conter component

--- src/components/AppCounter.vue
+++ src/components/AppCounter.vue
@@ -1,6 +1,23 @@
 <script>
 export default {};
 </script>
-<template></template>
+<template>
+  <div class="container-small">
+    <div class="card-number">
+      <span class="found-cards">Found 39 Cards</span>
+    </div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card-number {
+  background-color: black;
+  padding: 1.5rem;
+  margin-top: 1rem;
+  .found-cards {
+    color: white;
+    font-size: 1.2rem;
+    font-weight: 700;
+  }
+}
+</style>
