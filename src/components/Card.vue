0da5156b40ed51ea67a122ccb621a6a81cc46fdb
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-22 17:09:29 +0200

    add card images

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,7 +7,7 @@ export default {
 </script>
 <template>
   <div class="card">
-    <img :src="cardData.card_images.image_url" :alt="cardData.name" />
+    <img :src="cardData.card_images[0].image_url" :alt="cardData.name" />
     <h2>{{ cardData.name }}</h2>
     <div class="type">{{ cardData.archetype }}</div>
   </div>
@@ -19,6 +19,9 @@ export default {
   border: 1px solid brown;
   img {
     width: 100%;
+  }
+  h2 {
+    font-size: 0.7rem;
   }
 }
 </style>
