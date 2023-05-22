f1e8ad208396d916c4dc6b7d684e62f0cb2b13b9
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-22 15:17:38 +0200

    add appsearch structure in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import AppSearch from "./components/AppSearch.vue";
 
 export default {
   components: {
     AppHeader,
+    AppSearch,
   },
 };
 </script>
 
 <template>
   <AppHeader />
-  <main></main>
+  <main>
+    <AppSearch />
+  </main>
 </template>
 
 <style lang="scss">
